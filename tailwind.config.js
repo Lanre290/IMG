a6479747aeb5019f0f61cbbd9c54f2c6e1d3f455
Ashiru Olanrewaju <144792314+Lanre290@users.noreply.github.com>
2024-07-26 13:15:28 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+export default{
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
