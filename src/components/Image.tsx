23fcdd9c7fbfcf931d565726aa41ea798f14695b
Ashiru Olanrewaju <144792314+Lanre290@users.noreply.github.com>
2024-07-30 14:49:38 +0100

    Update Image.tsx remove excess margins

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,7 +9,7 @@ function Image(data:ImageFile){
     const styling = {...defaultStyle, ...data.style}
     return(
         <div 
-          className= "bg-center bg-cover bg-no-repeat w-2/6 h-200 px-20 sm:h-200 sm:w-100 md:h-200 cursor-pointer sm:h-200 sm:w-1/4 md:h-200 md:w-1/6 rounded-xl border border-gray-500 border-dashed flex flex-col items-center justify-center relative m-3" 
+          className= "bg-center bg-cover bg-no-repeat w-2/6 h-200 px-20 sm:h-200 sm:w-100 md:h-200 cursor-pointer sm:h-200 sm:w-1/4 md:h-200 md:w-1/6 rounded-xl border border-gray-500 border-dashed flex flex-col items-center justify-center relative" 
           style = {styling}
         >
             <button className="absolute top-4 right-4 px-4 py-2 cursor-pointer bg-transparent rounded-3xl text-red-700 text-3xl hover:bg-gray-700 hover:bg-opacity-40" onClick={data.onClose}>
